550764da09ba39c395040a4e9e77e1cf33c717db
varun3019 <varunrao1000@gmail.com>
2024-11-15 12:46:53 +0530

    11 commit

--- backend/server.js
+++ backend/server.js
@@ -7,32 +7,9 @@ import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 import cors from "cors";
 const app = express();
-const port = 4000;
-const allow = ["https://tomato-food-del-backend-gh2m.onrender.com/api/food/list"]
-app.use((req, res, next) => {
-    // Allow specific origin
-    res.setHeader("Access-Control-Allow-Origin",allow );
-
-    // Allow methods - you may want to reduce this based on your needs
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-
-    // Allow specific headers
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-
-    // Allow credentials for cookies or authentication
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-
-    // Set cache for preflight request
-    res.setHeader("Access-Control-Max-Age", 7200);
-
-    next();
-});
-
+const port =process.env.PORT || 4000;
 app.use(express.json());
-
+app.use(cors());
 
 connectDB();
 
